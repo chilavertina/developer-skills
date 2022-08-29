dc71d08a6261dfef15c89923a9b4d76fa7634cad
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-08-29 13:35:45 +0200

    upotreba debuger-a

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const calcTempAmplitude = function (temps) {
 const amplitude = calcTempAmplitude(temperatures);
 console.log(amplitude);
 */
-
+/*
 const calcTempAmplitudeNew = function (t1, t2) {
     const temps = t1.concat(t2);
     console.log(temps);
@@ -49,3 +49,43 @@ const calcTempAmplitudeNew = function (t1, t2) {
 
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(amplitudeNew);
+*/
+
+const measureKelvin = function() {
+    const measurement = {
+        type: 'temp',
+        unit: 'celsius',
+        //value: Number(prompt('Degrees celsius:'))
+        value: 10,
+    }
+
+    console.table(measurement);
+
+    //console.log(measurement.value);
+
+    const kelvin = measurement.value + 273;
+    return kelvin;
+}
+
+console.log(measureKelvin());
+
+const calcTempAmplitudeBug = function (t1, t2) {
+    const temps = t1.concat(t2);
+    console.log(temps);
+
+    let max = 0;    
+    let min = 0;
+
+    for(let i = 0; i < temps.length; i++) {
+        const curTemp = temps[i];
+        if (typeof curTemp !== 'number') continue;
+
+        if (curTemp > max) max = curTemp;
+        if (curTemp < min) min = curTemp;
+    }
+    console.log(max, min);
+    return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+console.log(amplitudeBug);
